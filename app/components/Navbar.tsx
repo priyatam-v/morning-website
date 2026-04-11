b917300b589a0cfcd385c71ed8f4a4a9d6a94b4c
priyatam-v <vinnakota4201@gmail.com>
2026-04-11 21:35:38 +0530

    feat: use SVG logo in Navbar, update waitlist section index to 4

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,18 +3,17 @@ import styles from './Navbar.module.css'
 
 export default function Navbar() {
   const scrollToWaitlist = () => {
-    window.dispatchEvent(new CustomEvent('navigate-to-section', { detail: { index: 5 } }))
+    window.dispatchEvent(new CustomEvent('navigate-to-section', { detail: { index: 4 } }))
   }
 
   return (
     <nav className={`${styles.nav} ${styles.visible}`}>
       <div className={styles.inner}>
-        <div className={styles.logo}>
-          <span className={styles.logoText}>Morn</span>
-          <span className={styles.logoI}>ı</span>
-          <span className={styles.logoDot} />
-          <span className={styles.logoText}>ng</span>
-        </div>
+        <img
+          src="/Morning_Logo.svg"
+          alt="Morning"
+          className={styles.logo}
+        />
         <button className={styles.cta} onClick={scrollToWaitlist}>
           Join Waitlist
         </button>
